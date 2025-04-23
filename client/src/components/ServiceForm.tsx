924bfeb9e36f0d0af9b950c75f82c78fd1a04b79
{your_name} <{Caleb.feliciano11@gmial.com}>
2025-04-23 18:25:10 -0400

    Service page updates

---
+++ client/src/components/ServiceForm.tsx
@@ -0,0 +1,11 @@
+const ServiceForm = () => {
+    return (
+      <form>
+        <h3>New Service</h3>
+        {/* Add form fields here */}
+      </form>
+    );
+  };
+  
+  export default ServiceForm;
+  
